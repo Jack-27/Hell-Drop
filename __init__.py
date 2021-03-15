40e216f106a28f6c94bf6a2766e82823d75fb4c4
Jack <oshowot23@gmail.com>
2021-03-14 20:55:15 -0600

    haba

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, redirect, request, session
-from admin import admin
+from webApp.admin import admin
 from flask_sqlalchemy import SQLAlchemy
-from models import db, Product, Order, Stock
+from webApp.models import db, Product, Order, Stock
 import os
 #! /usr/bin/env python3.6
 
